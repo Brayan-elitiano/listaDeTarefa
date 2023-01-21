f8e669b9a59eb4e7a98728b7bb9e062419630703
brayan  mauricio kedroski da silva <116443973+Brayan-elitiano@users.noreply.github.com>
2023-01-21 11:35:20 -0300

    adicionei extencaoes

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function add() {
-    const ul = document.getElementById("lista")
-    const input = document.getElementById("input")
-    const tarefa = input.value
-    const li = document.createElement("li")
-    li.innerText = tarefa
-    ul.appendChild(li)
-    input.value= ""
-    
-        
+  const ul = document.getElementById("lista");
+  const input = document.getElementById("input");
+  const tarefa = input.value;
+  const li = document.createElement("li");
+  li.innerText = tarefa;
+  ul.appendChild(li);
+  input.value = "";
 }
+
