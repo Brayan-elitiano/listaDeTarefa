d9b9f4019579e0df15b50f71bfeb3b937d800c0e
brayan-elitiano <brayan9000mauricio@gmail.com>
2023-01-24 16:28:45 -0300

    commitin

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+const tarefas = ["estudar HTML"];
+
 function add() {
-  const ul = document.getElementById("lista");
-  const input = document.getElementById("input");
+  const input = document.querySelector("input");
   const tarefa = input.value;
-  const li = document.createElement("li");
-  li.innerText = tarefa;
-  ul.appendChild(li);
+  tarefas.push(tarefa)
   input.value = "";
+  render()
 }
 
+function render() {
+  ul.
+  const ul = document.querySelector("ul");
+
+  tarefas.forEach(function(tarefa){
+    const li = document.createElement("li");
+    li.innerText = tarefa;
+    ul.appendChild(li);
+    console.log(tarefa);
+  });
+}
+render();
