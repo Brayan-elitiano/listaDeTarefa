e6764cb772d741ab5a05cbb7c47bb491a0329ec5
brayan-elitiano <brayan9000mauricio@gmail.com>
2023-01-31 16:34:45 -0300

    alteracao de todo o projeto

--- script.js
+++ script.js
@@ -1,28 +1,67 @@
-const usuario = "";
-const senha = "";
+const senha = prompt("crie a senha");
+const show = prompt("Digite a senha");
 
+if(show == senha){
+    const tarefas = [] // []-> array
 
-
-const tarefas = ["estudar HTML"];
-
-function add() {
-  const input = document.querySelector("input");
-  const tarefa = input.value;
-  tarefas.push(tarefa)
-  input.value = "";
-  render()
+function add(){
+    //const input = document.querySelector("input");
+    //const tarefa = input.value
+    //if (tarefa) { // se o campo está vazio ele já dá falso
+    //    tarefas.push(tarefa) //adiciona nova tarefa
+    //    input.value = "" //barra do input "limpa"
+    //    render()
+    //}
+    //outro jeito de fazer
+    const input = document.getElementById("valor")
+    const tarefa = input.value
+    if (!tarefa) {
+        alert("escreva uma tarefa")
+        return
+    }
+    tarefas.push(tarefa) //adiciona nova tarefa
+    input.value = "" //barra do input "limpa"
+    
+    render()
 }
 
-function render() {
-  
-  const ul = document.querySelector("ul");
-  ul.innerHTML = null
-  tarefas.forEach(function(tarefa){
+function render(){ 
+    
+    const ol = document.querySelector("ol")
+    ol.innerHTML = null // o conteúdo html dentro da ol é nulo
+    tarefas.forEach(function(tarefa){
+        const li = document.createElement("li") //criando um novo li
+        li.innerText = tarefa //tradução: o texto dentro de li é a tarefa(o valor dentro do input)
+        
+        const input2 = document.createElement("input")
+        li.appendChild(input2)
 
-    const li = document.createElement("li");
-    li.innerText = tarefa;
-    ul.appendChild(li);
-    console.log(tarefa);
-  });
+        const editar = document.createElement("button")
+        editar.innerText="editar"
+        li.appendChild(editar)
+        editar.addEventListener("click", function(){
+            const tarefaNova = input2.value
+            const index = tarefas.indexOf(tarefa);
+            if (index !== -1) {
+                tarefas[index] = tarefaNova;
+            }
+            render()
+        })
+
+        const excluir = document.createElement("button")
+        excluir.innerText = "apagar"
+        li.appendChild(excluir)
+        excluir.addEventListener("click", function(){
+            const apagar = tarefas.indexOf(tarefa)
+            tarefas.splice(apagar,1)
+            render()
+        });
+
+        ol.appendChild(li)// fazer a tarefa aparecer dentro da lista
+        
+    })
 }
-render();
+render()
+}else{
+    alert("senha")
+}
--- teste/Table1.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE [dbo].[Table1]
-(
-  [Id] INT NOT NULL PRIMARY KEY
-  [nome] VARCHAR NOT NULL 
-)
